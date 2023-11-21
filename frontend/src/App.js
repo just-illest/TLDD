171a64b3411bdfb51122d7c33e9d2fc1f58e7672
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-11-21 15:24:23 +0530

    AAAA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
-import { ImageUpload } from "./home";
+import Predict from "./Predict";
+import{Link} from "react-router-dom";
+import Navbar from "./navbar";
 
 function App() {
   // return <ImageUpload />;
   return(
     <>
-    <div className="fixed flex w-screen h-32 items-center text-indigo text-xl font-helvetica">
-      
-      <div className="flex w-full h-16 justify-center items-center"><div className="flex h-full w-3/4 justify-center items-center">DISEASES</div></div>
-      <div className="flex w-full h-16 justify-center items-center"><div className="flex h-full w-3/4 justify-center items-center">CURES</div></div>
-      <div className="flex w-full h-16 justify-center items-center"><div className="flex h-full w-3/4 justify-center items-center">HOW TO USE</div></div>
-      <div className="flex w-full h-16 justify-center items-center"><div className="flex h-full w-3/4 justify-center items-center">HELP US IMPROVE</div></div>
-      <div className="flex w-full h-16 justify-center items-center"></div>
-      <div className="flex w-full h-16 justify-center items-center"></div>
-      <div className="flex w-full h-16 justify-center items-center"></div>
-    </div>
+    <div className="absolute"><Navbar/></div>
+
     <div className="flex pt-20 h-screen w-screen bg-slate font-helvetica items-center justify-center">
       <div className="relative flex h-4/5 w-3/5 items-center justify-end">
         <div className="peer flex border-2 border-black
@@ -27,14 +21,14 @@ function App() {
       <div className="py-20 pr-36 pl-2 text-indigo h-4/5 w-2/5 text-7xl">
         A better way to spot diseases for tomatoes
         <div>
-        <button  className=" h-20 w-32 text-xl text-neutral text-center
+        <button className=" h-20 w-32 text-xl text-neutral text-center
         bg-indigo
-        hover:bg-neutral hover:text-indigo duration-300" >
-          Get Started</button>
+        hover:bg-neutral hover:text-indigo duration-300" > Get Started
+          </button>
         </div>
       </div>
-      
     </div>
+    <Predict/>
     </>
   );
 
