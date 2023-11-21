4cd1a8540eb0f3b183c4bfc0cc2b615174d8bae5
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-11-21 18:07:52 +0530

    Added prediction page

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
-import Navbar from "./navbar";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import { Paper, CardActionArea, CardMedia, Grid, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress } from "@material-ui/core";
+import { Paper, CardActionArea, CardMedia, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress, List } from "@material-ui/core";
 import { DropzoneArea } from 'material-ui-dropzone';
 import { common } from '@material-ui/core/colors';
 import Clear from '@material-ui/icons/Clear';
@@ -26,118 +25,89 @@ const ColorButton = withStyles((theme) => ({
 // const axios = require("axios").default;
 
 const useStyles = makeStyles((theme) => ({
-  // grow: {
-  //   flexGrow: 1,
-  // },
-  // clearButton: {
-  //   width: "-webkit-fill-available",
-  //   borderRadius: "15px",
-  //   padding: "15px 22px",
-  //   color: "#000000a6",
-  //   fontSize: "20px",
-  //   fontWeight: 900,
-  // },
-  // root: {
-  //   maxWidth: 345,
-  //   flexGrow: 1,
-  // },
-  // media: {
-  //   height: 400,
-  // },
-  // paper: {
-  //   padding: theme.spacing(2),
-  //   margin: 'auto',
-  //   maxWidth: 500,
-  // },
-  // gridContainer: {
-  //   justifyContent: "center",
-  //   padding: "4em 1em 0 1em",
-  // },
-  // mainContainer: {
-  //   backgroundImage: `url(${image})`,
-  //   backgroundRepeat: 'no-repeat',
-  //   backgroundPosition: 'center',
-  //   backgroundSize: 'cover',
-  //   height: "93vh",
-  //   marginTop: "8px",
-  // },
-  // imageCard: {
-  //   margin: "auto",
-  //   maxWidth: 400,
-  //   height: 500,
-  //   backgroundColor: 'transparent',
-  //   boxShadow: '0px 9px 70px 0px rgb(0 0 0 / 30%) !important',
-  //   borderRadius: '15px',
-  // },
-  // imageCardEmpty: {
-  //   height: 'auto',
-  // },
-  // noImage: {
-  //   margin: "auto",
-  //   width: 400,
-  //   height: "400 !important",
-  // },
-  // input: {
-  //   display: 'none',
-  // },
-  // uploadIcon: {
-  //   background: 'white',
-  // },
-  // tableContainer: {
-  //   backgroundColor: 'transparent !important',
-  //   boxShadow: 'none !important',
-  // },
-  // table: {
-  //   backgroundColor: 'transparent !important',
-  // },
-  // tableHead: {
-  //   backgroundColor: 'transparent !important',
-  // },
-  // tableRow: {
-  //   backgroundColor: 'transparent !important',
-  // },
-  // tableCell: {
-  //   fontSize: '22px',
-  //   backgroundColor: 'transparent !important',
-  //   borderColor: 'transparent !important',
-  //   color: '#000000a6 !important',
-  //   fontWeight: 'bolder',
-  //   padding: '1px 24px 1px 16px',
-  // },
-  // tableCell1: {
-  //   fontSize: '14px',
-  //   backgroundColor: 'transparent !important',
-  //   borderColor: 'transparent !important',
-  //   color: '#000000a6 !important',
-  //   fontWeight: 'bolder',
-  //   padding: '1px 24px 1px 16px',
-  // },
-  // tableBody: {
-  //   backgroundColor: 'transparent !important',
-  // },
-  // text: {
-  //   color: 'white !important',
-  //   textAlign: 'center',
-  // },
-  // buttonGrid: {
-  //   maxWidth: "416px",
-  //   width: "100%",
-  // },
-  // detail: {
-  //   backgroundColor: 'white',
-  //   display: 'flex',
-  //   justifyContent: 'center',
-  //   flexDirection: 'column',
-  //   alignItems: 'center',
-  // },
-  // appbar: {
-  //   background: '#be6a77',
-  //   boxShadow: 'none',
-  //   color: 'white'
-  // },
-  // loader: {
-  //   color: '#be6a77 !important',
-  // }
+  clearButton: {
+    width : "100%",
+    color: "#05386B",
+    fontSize: "20px",
+    fontWeight: 900,
+    backgroundColor: '#5CDB95',
+    borderRadius: 0,
+  },
+  root: {
+    maxWidth: 345,
+    flexGrow: 1,
+  },
+  media: {
+    height: 400,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  
+  imageCard: {
+    height: 500,
+    backgroundColor: 'transparent',
+    borderRadius: 0,
+  },
+  imageCardEmpty: {
+    height: 'auto'
+  },
+  noImage: {
+    margin: "auto",
+    width: 400,
+    height: "400 !important",
+  },
+  input: {
+    display: 'none',
+  },
+  uploadIcon: {
+    background: 'white',
+  },
+  tableContainer: {
+    backgroundColor: '#5CDB95',
+    boxShadow: 'none !important',
+  },
+  table: {
+    backgroundColor: '#5CDB95',
+  },
+  tableHead: {
+    backgroundColor: 'transparent !important',
+  },
+  tableRow: {
+    backgroundColor: 'transparent !important',
+  },
+  tableCell: {
+    fontSize: '22px',
+    backgroundColor: 'transparent !important',
+    borderColor: 'transparent !important',
+    color:'#05386B',
+  },
+  tableCell1: {
+    fontSize: '14px',
+    backgroundColor: 'transparent !important',
+    borderColor: 'transparent !important',
+    color:'#05386B',
+  },
+  tableBody: {
+    backgroundColor: 'transparent !important',
+  },
+  text: {
+    color: 'white !important',
+    textAlign: 'center',
+  },
+  detail: {
+    backgroundColor: '#5CDB95',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: 0,
+  },
+  loader: {
+    color: '#be6a77 !important',
+  }
 }));
 export const ImageUpload = () => {
   const classes = useStyles();
@@ -207,70 +177,86 @@ export const ImageUpload = () => {
 
   return (
     <>
-      <Navbar/>
-      <Container maxWidth={false} className="bg-slate h-screen w-screen" disableGutters={true}>
-        <Grid
-          className="bg-emerald"
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          spacing={2}
-        >
-          <Grid item xs={12}>
-            <Card className={`${classes.imageCard} ${!image ? classes.imageCardEmpty : ''}`}>
-              {image && <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={preview}
-                  component="image"
-                  title="Contemplative Reptile"
-                />
-              </CardActionArea>
-              }
-              {!image && <CardContent className="h-4/5 w-4/5">
-                <DropzoneArea
-                  acceptedFiles={['image/*']}
-                  dropzoneText={"Drag and drop an image of a potato plant leaf to process"}
-                  onChange={onSelectFile}
-                />
-              </CardContent>}
-              {data && <CardContent className={classes.detail}>
-                <TableContainer component={Paper} className={classes.tableContainer}>
-                  <Table className={classes.table} size="small" aria-label="simple table">
-                    <TableHead className={classes.tableHead}>
-                      <TableRow className={classes.tableRow}>
-                        <TableCell className={classes.tableCell1}>Label:</TableCell>
-                        <TableCell align="right" className={classes.tableCell1}>Confidence:</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody className={classes.tableBody}>
-                      <TableRow className={classes.tableRow}>
-                        <TableCell component="th" scope="row" className={classes.tableCell}>
-                          {data.class}
-                        </TableCell>
-                        <TableCell align="right" className={classes.tableCell}>{confidence}%</TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </CardContent>}
-              {isLoading && <CardContent className={classes.detail}>
-                <CircularProgress color="secondary" className={classes.loader} />
-                <Typography className={classes.title} variant="h6" noWrap>
-                  Processing
-                </Typography>
-              </CardContent>}
-            </Card>
-          </Grid>
+      <Container maxWidth={false} className="bg-slate h-screen w-screen font-helvetica" disableGutters={true}>
+        <div className="flex ">
+          <div className="flex flex-col
+          bg-slate  
+          px-20 h-[768px]">
+            <div className=" w-full ">
+              <Card className={`${classes.imageCard} ${!image ? classes.imageCardEmpty : ''}`}>
+                {image && <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={preview}
+                    component="image"
+                    title="Contemplative Reptile"
+                  />
+                </CardActionArea>
+                }
+                {!image && <>
+                  <CardContent className="bg-slate border-2 border-neutral rounded-none w-[90vw]">
+                    <DropzoneArea
+                      acceptedFiles={['image/*']}
+                      dropzoneText={"Drag and drop an image of a tomato plant"}
+                      onChange={onSelectFile}
+                    />
+                  </CardContent>
+                  <div className="text-xl text-neutral mt-20">
+                    To get the <u>best</u> result, adhere to the following:
+                    <List>
+                      <li>- Take a picture of a single leaf</li>
+                      <li>- Make sure leaf is properly lit</li>
+                      <li>- Make sure leaf is properly lit</li>
+                    </List>
+                  </div>
+                  </>
+                }
+                {data && <CardContent className={classes.detail}>
+                  <TableContainer component={Paper} className={classes.tableContainer}>
+                    <Table className={classes.table} size="small" aria-label="simple table">
+                      <TableHead className={classes.tableHead}>
+                        <TableRow className={classes.tableRow}>
+                          <TableCell className={classes.tableCell1}>Label:</TableCell>
+                          <TableCell align="right" className={classes.tableCell1}>Confidence:</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody className={classes.tableBody}>
+                        <TableRow className={classes.tableRow}>
+                          <TableCell component="th" scope="row" className={classes.tableCell}>
+                            {data.class}
+                          </TableCell>
+                          <TableCell align="right" className={classes.tableCell}>{confidence}%</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </CardContent>}
+                {isLoading && <CardContent className={classes.detail}>
+                  <CircularProgress color="secondary" className={classes.loader} />
+                  <Typography className={classes.title} variant="h6" noWrap>
+                    Processing
+                  </Typography>
+                </CardContent>}
+              </Card>
+            </div>
+            {data &&
+              <div className=" pt-4" >
+                <ColorButton variant="contained" className={classes.clearButton} color="primary" component="span" size="large" onClick={clearData} startIcon={<Clear fontSize="large" />}>
+                  Clear
+                </ColorButton>
+              </div>}
+          </div>
           {data &&
-            <Grid item className={classes.buttonGrid} >
-
-              <ColorButton variant="contained" className={classes.clearButton} color="primary" component="span" size="large" onClick={clearData} startIcon={<Clear fontSize="large" />}>
-                Clear
-              </ColorButton>
-            </Grid>}
-        </Grid >
+            <div className="flex-row pr-20 space-y-2 h-full w-full  font-helvetica text-neutral text-8xl"> 
+              <div>{data.class}</div> 
+              <div className="text-xl"> What is bacterial spot?  Bacterial spot of tomato 
+              is a potentially devastating disease that, in severe cases, can lead to 
+              unmarketable fruit and even plant death.  Bacterial spot can occur wherever 
+              tomatoes are grown, but is found most frequently in warm, wet climates, as 
+              well as in greenhouses.</div>
+            </div>
+          }
+        </div>
       </Container >
     </>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors:{
-      'slate':'#5CDB95',
+      'indigo':'#5CDB95',
       'teal':'#8EE4AF',
       'neutral':'#EDF5E1',
       'emerald':'#379683',
-      'indigo':'#05386B',
+      'slate':'#05386B',
       'black':'#000000',
       'white' : '#FFFFFF',
     },
